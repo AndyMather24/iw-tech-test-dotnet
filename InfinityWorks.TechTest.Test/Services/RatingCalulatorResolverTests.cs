8dedb7ef868f4fda30b525e95b38fcc25631659d
Andrew Mather <Andy-mather@live.co.uk>
2022-05-04 22:14:43 +0100

    FHIS resolver test

--- InfinityWorks.TechTest.Test/Services/RatingCalulatorResolverTests.cs
+++ InfinityWorks.TechTest.Test/Services/RatingCalulatorResolverTests.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using InfinityWorks.TechTest.Enum;
 using InfinityWorks.TechTest.Services;
-using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using NUnit.Framework;
+
 namespace InfinityWorks.TechTest.Test.Services
 {
 	[TestFixture]
@@ -30,6 +30,21 @@ namespace InfinityWorks.TechTest.Test.Services
 
             // Assert
             Assert.IsInstanceOf<FHRSRatings>(result);
+
+		}
+
+
+		[Test]
+		public void Resolve_FHISRatingSchema_ReturnsFHISRatingType()
+		{
+			// Arrange
+			var resolver = new RatingCalulatorResolver(_serviceProviderMock.Object);
+
+			// Act
+			var result = resolver.Resolve(RatingSchema.FHIS);
+
+			// Assert
+			Assert.IsInstanceOf<FHISRatings>(result);
 
 		}
 	}
